63c0d4810a1f609ce24441128fd666770662d087
Fabian Margreiter <fabian.margreiter@gmail.com>
2024-02-08 01:54:53 +0100

    Added update and delete operation for flights

--- flightlog/flight.py
+++ flightlog/flight.py
@@ -14,6 +14,7 @@ def index():
         """
         SELECT
             ROW_NUMBER() OVER (ORDER BY f.date ASC, f.id ASC) as flight_no,
+            f.id as id,
             f.date as date,
             wm.name as wing_manufacturer,
             wt.name as wing_name,
@@ -24,7 +25,8 @@ def index():
             (f.duration_minutes / 60) || "h " || (f.duration_minutes % 60) || "m" as duration,
             ft.name as flight_type,
             f.hike_and_fly as hike_and_fly,
-            f.with_skies as with_skies
+            f.with_skies as with_skies,
+            f.comment as comment
         FROM flight f
             JOIN wing w ON f.wing_id = w.id
             JOIN wing_type wt ON w.wing_type_id = wt.id
@@ -173,3 +175,155 @@ def create():
         flight_types=flight_types,
         today=datetime.now().strftime("%Y-%m-%d"),
     )
+
+
+def get_flight(id):
+    flight = (
+        get_db()
+        .execute(
+            """
+        SELECT
+            f.id as id,
+            f.date as date,
+            w.id as wing_id,
+            laus.id as launch_site_id,
+            lans.id as landing_site_id,
+            ft.id as flight_type_id,
+            f.hike_and_fly as hike_and_fly,
+            f.with_skies as with_skies,
+            f.duration_minutes as duration_minutes,
+            f.comment as comment
+        FROM flight f
+            JOIN wing w ON f.wing_id = w.id
+            JOIN wing_type wt ON w.wing_type_id = wt.id
+            JOIN wing_manufacturer wm ON wt.wing_manufacturer_id = wm.id
+            JOIN site laus ON f.launch_site_id = laus.id
+            JOIN site lans ON f.landing_site_id = lans.id
+            JOIN flight_type ft ON f.flight_type_id = ft.id
+        WHERE f.id = ?
+        """,
+            (id,),
+        )
+        .fetchone()
+    )
+
+    if flight is None:
+        abort(404, f"Flight ID {id} doesn't exist.")
+
+    return flight
+
+
+@flight.route("/<int:id>/update", methods=("GET", "POST"))
+def update(id):
+    flight = get_flight(id)
+
+    if request.method == "POST":
+        date = request.form["date"]
+        wing_id = int(request.form["wing"])
+        launch_site_id = int(request.form["launch_site"])
+        landing_site_id = int(request.form["landing_site"])
+        flight_type_id = int(request.form["flight_type"])
+        hike_and_fly = True if "hike_and_fly" in request.form else False
+        with_skies = True if "with_skies" in request.form else False
+        comment = request.form["comment"]
+        duration = None if request.form["duration"] == "" else int(request.form["duration"])
+
+        db = get_db()
+        db.execute(
+            """
+                UPDATE flight
+                SET date = ?,
+                    wing_id = ?,
+                    launch_site_id = ?,
+                    landing_site_id = ?,
+                    flight_type_id = ?,
+                    hike_and_fly = ?,
+                    with_skies = ?,
+                    duration_minutes = ?,
+                    comment = ?
+                WHERE id = ?
+            """,
+            (
+                date,
+                wing_id,
+                launch_site_id,
+                landing_site_id,
+                flight_type_id,
+                hike_and_fly,
+                with_skies,
+                duration,
+                comment,
+                id,
+            ),
+        )
+        db.commit()
+        return redirect(url_for("flight.index"))
+
+    db = get_db()
+    wings = db.execute(
+        """
+        SELECT
+            w.id as id,
+            wm.name as manufacturer,
+            wt.name as type,
+            w.size_designator as size_designator
+        FROM wing w
+            JOIN wing_type wt ON w.wing_type_id = wt.id
+            JOIN wing_manufacturer wm ON wt.wing_manufacturer_id = wm.id
+        ORDER BY wm.name ASC, wt.name ASC, w.size_designator ASC
+        """
+    ).fetchall()
+    launch_sites = db.execute(
+        """
+        SELECT
+            s.id as id,
+            s.name as name,
+            c.shorty as country,
+            s.elevation as elevation
+        FROM site s
+            JOIN country c ON s.country_id = c.id
+        WHERE
+            s.is_launch = 1
+        ORDER BY s.name ASC, c.shorty ASC
+        """
+    ).fetchall()
+    landing_sites = db.execute(
+        """
+        SELECT
+            s.id as id,
+            s.name as name,
+            c.shorty as country,
+            s.elevation as elevation
+        FROM site s
+            JOIN country c ON s.country_id = c.id
+        WHERE
+            s.is_landing = 1
+        ORDER BY s.name ASC, c.shorty ASC
+        """
+    ).fetchall()
+    flight_types = db.execute(
+        """
+        SELECT
+            ft.id, ft.name
+        FROM flight_type ft
+        ORDER BY ft.name ASC
+        """
+    ).fetchall()
+
+    return render_template(
+        "flight/update.html",
+        flight=flight,
+        wings=wings,
+        launch_sites=launch_sites,
+        landing_sites=landing_sites,
+        flight_types=flight_types,
+    )
+
+
+@flight.route("/<int:id>/delete", methods=("POST",))
+def delete(id):
+    get_flight(id)
+    db = get_db()
+    db.execute("DELETE FROM flight WHERE id = ?", (id,))
+    db.commit()
+    return redirect(url_for("flight.index"))
