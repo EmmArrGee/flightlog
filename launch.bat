be454ffae5b97f8fa7ebd72d7be1c4edee4919e0
Fabian Margreiter <fabian.margreiter@gmail.com>
2024-02-22 14:53:26 +0100

    Initialized tailwindcss

--- launch.bat
+++ launch.bat
@@ -1,3 +1,4 @@
 CALL .venv\Scripts\activate
 start "" http://127.0.0.1:5000
-flask --app flightlog run
+start npm run create-css
+flask --app flightlog run --debug
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: ["./flightlog/templates/**/*.{html,htm}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
